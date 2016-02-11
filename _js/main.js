a421e580a0b227592ba6b09b7d1137423241be8d
Bob Small <bob.small@csaa.com>
2016-02-11 16:10:37 -0700

    Changed reporting cards to clean up

--- _js/main.js
+++ _js/main.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
     cards();
 
 var status = "green";
+/* Status
+   Full Security Test:
+       Green = has been tested within the last 6 months
+       Yellow = has not been tested in the last 6 months
+       Red = has not been tested in the last 2 years
+       Grey = has not been tested
+   Static Code
+       Green = has been tested in the last 5 days
+       Yellow = has not been tested in the last 5 days
+       Red = has not been tested within the last 2 months 
+*/ 
+
 
 function timeSince(date) {
 
@@ -10,30 +22,43 @@ function timeSince(date) {
     var interval = Math.floor(seconds / 31536000);
 
     if (interval > 1) {
-	status = "yellow";
+	status1 = "red";
+	status = "red";
         return interval + " years";
     }
     interval = Math.floor(seconds / 2592000);
     if (interval > 1) {
-        status = "yellow";
-        return interval + " months";
+        if (interval > 5) { 
+            status1 = "yellow"; 
+        } else { 
+            status1 = "green" 
+        }
+        status = "red";
+    return interval + " months";
     }
     interval = Math.floor(seconds / 86400);
     if (interval > 1) {
-        if (interval > 5) { status = "yellow"; } else { status = "green" }
+        status1 = "green";
+        if (interval > 5) { 
+           status = "yellow"; 
+        } else { 
+           status = "green"; 
+        }
         return interval + " days";
     }
     interval = Math.floor(seconds / 3600);
     if (interval > 1) {
+        status1 = "green";
         status = "green";
         return interval + " hours";
     }
     interval = Math.floor(seconds / 60);
     if (interval > 1) {
+        status1 = "green";
         status = "green";
         return interval + " minutes";
     }
-    status = "green";
+    status1 = "green";
     return Math.floor(seconds) + " seconds";
 }
 
@@ -44,18 +69,18 @@ function cards() {
 	var key = mydata.project.length;
 	var text = "";
 
-	for (i=0; i<key; i++) {
+	for (i=0; i < key; i++) {
            var timeAgo = 0;
            timeAgo = timeSince(mydata.project[i].date);
-	   if (timeAgo.search("month") > -1) {
-	        status1 = "yellow";
-           } else {
-                status1 = "blue";
+           timeAgo = timeAgo + " ago";
+           if (timeAgo.search("NaN") > -1) {
+                status1 = "grey";
+                timeAgo = "TBT";
            }
            text += '<div id="card" class="w3-card-4"><header class="w3-container w3-' +
                 status1+ '"><h1><center>' +
                 mydata.project[i].title + '</center></h1></header><br><div id="date_lable" class="w3-container">' +
-                timeAgo + ' ago</div><div id="issue_lable" class="w3-container">' +
+                timeAgo + '</div><div id="issue_lable" class="w3-container">' +
                 mydata.project[i].issues + '</div> </div>';
         }
         document.getElementById("card").innerHTML = text;
@@ -65,7 +90,7 @@ function cards() {
 	var key = mydata.project.length;
 	var text = "";
 
-	for (i=0; i<key; i++) {
+	for (i = 0; i < key - 1; i++) {
 
            var timeAgo = 0;
            timeAgo = timeSince(mydata.project[i].date);
--- json/makeJSON.sh
+++ json/makeJSON.sh
@@ -1,1 +1,1 @@
-rm jenkins1.json;echo "{ \"project\" : [" > jenkins1.js;cat *.json>> jenkins1.js;echo "] }" >> jenkins1.js;mv jenkins1.js jenkins1.json
+mv ./secTests.json ./secTests.txt;rm jenkins1.json;echo "{ \"project\" : [" > jenkins1.js;cat *.json>> jenkins1.js;echo "{}] }" >> jenkins1.js;mv jenkins1.js jenkins1.json;mv ./secTests.txt ./secTests.json
